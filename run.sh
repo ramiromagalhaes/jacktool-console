826d7457557ab71994054bbdc9c61601a44adc75
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-08-17 17:31:42 -0300

    Some folders that should be processed.

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-jacktool-console /mnt /home/ramiro/Imagens/2010-03-29-Relampagos /home/ramiro/Imagens/2011-09-10-MalfadineTricoloreAoRagoutDeCogumelos
+jacktool-console /mnt \
+/home/ramiro/Imagens/2006-12-17-Zoologico \
+/home/ramiro/Imagens/2007-08-04-IlhaGrande \
+/home/ramiro/Imagens/2009-03-08-Escalada \
+/home/ramiro/Imagens/2009-12-31-Gramado \
+/home/ramiro/Imagens/2010-03-29-Relampagos \
+/home/ramiro/Imagens/2010-10-18-LuaDeMel \
+/home/ramiro/Imagens/2011-09-03-FazendaAntonia \
+/home/ramiro/Imagens/2011-09-10-MalfadineTricoloreAoRagoutDeCogumelos
