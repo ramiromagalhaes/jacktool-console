ccc7af2c2bf7b763a867b69cb62c39c9736bad8a
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-10 10:06:53 -0300

    Changed markings paths.

--- run.sh
+++ run.sh
@@ -1,24 +1,23 @@
 ./jacktool-console -o/mnt/{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39} \
--i/home/ramiro/Imagens/2006-12-17-Zoologico.jacktool \
--i/home/ramiro/Imagens/2007-08-04-IlhaGrande.jacktool \
--i/home/ramiro/Imagens/2009-03-08-Escalada.jacktool \
--i/home/ramiro/Imagens/2009-12-31-Gramado.jacktool \
--i/home/ramiro/Imagens/2010-03-29-Relampagos.jacktool \
--i/home/ramiro/Imagens/2010-10-18-LuaDeMel-{10-18,10-19,10-20,10-21,10-22,10-23,10-24,10-25,10-26,10-27,10-28,10-29,10-30,10-31,11-01,11-02,11-03,11-04,11-05,11-06}.jacktool \
--i/home/ramiro/Imagens/2011-09-03-FazendaAntonia.jacktool \
--i/home/ramiro/Imagens/2011-09-10-MalfadineTricoloreAoRagoutDeCogumelos.jacktool \
--i/home/ramiro/Imagens/2006-10-12-ViagemRecife.jacktool \
--i/home/ramiro/Imagens/2006-10-30-ViagemMinasGerais.jacktool \
--i/home/ramiro/Imagens/2008-01-13-JardimBotânico.jacktool \
--i/home/ramiro/Imagens/2008-03-01-MergulhoComAndrea.jacktool \
--i/home/ramiro/Imagens/2011-12-13-ViagemAosEUA.jacktool \
--i/home/ramiro/Imagens/2013-05-25-NiverLipeNanda.jacktool \
--i/home/ramiro/Imagens/2006-07-29-AntiguidadesYuri.jacktool \
--i/home/ramiro/Imagens/2006-09-10-Parapente.jacktool \
--i/home/ramiro/Imagens/2007-02-03-WorkshopComposicao.jacktool \
--i/home/ramiro/Imagens/2007-02-20-RioWaterPlanet.jacktool \
--i/home/ramiro/Imagens/2007-04-21-RedBullAirRace.jacktool \
--i/home/ramiro/Imagens/2008-05-31-Pizza.jacktool \
--i/home/ramiro/Imagens/2008-08-01-Argentina-album.jacktool \
--i/home/ramiro/Imagens/2008-12-31-Caxambu.jacktool
-
+-i/home/ramiro/workspace/markings/2006-12-17-Zoologico.jacktool \
+-i/home/ramiro/workspace/markings/2007-08-04-IlhaGrande.jacktool \
+-i/home/ramiro/workspace/markings/2009-03-08-Escalada.jacktool \
+-i/home/ramiro/workspace/markings/2009-12-31-Gramado.jacktool \
+-i/home/ramiro/workspace/markings/2010-03-29-Relampagos.jacktool \
+-i/home/ramiro/workspace/markings/2010-10-18-LuaDeMel-{10-18,10-19,10-20,10-21,10-22,10-23,10-24,10-25,10-26,10-27,10-28,10-29,10-30,10-31,11-01,11-02,11-03,11-04,11-05,11-06}.jacktool \
+-i/home/ramiro/workspace/markings/2011-09-03-FazendaAntonia.jacktool \
+-i/home/ramiro/workspace/markings/2011-09-10-MalfadineTricoloreAoRagoutDeCogumelos.jacktool \
+-i/home/ramiro/workspace/markings/2006-10-12-ViagemRecife.jacktool \
+-i/home/ramiro/workspace/markings/2006-10-30-ViagemMinasGerais.jacktool \
+-i/home/ramiro/workspace/markings/2008-01-13-JardimBotânico.jacktool \
+-i/home/ramiro/workspace/markings/2008-03-01-MergulhoComAndrea.jacktool \
+-i/home/ramiro/workspace/markings/2011-12-13-ViagemAosEUA.jacktool \
+-i/home/ramiro/workspace/markings/2013-05-25-NiverLipeNanda.jacktool \
+-i/home/ramiro/workspace/markings/2006-07-29-AntiguidadesYuri.jacktool \
+-i/home/ramiro/workspace/markings/2006-09-10-Parapente.jacktool \
+-i/home/ramiro/workspace/markings/2007-02-03-WorkshopComposicao.jacktool \
+-i/home/ramiro/workspace/markings/2007-02-20-RioWaterPlanet.jacktool \
+-i/home/ramiro/workspace/markings/2007-04-21-RedBullAirRace.jacktool \
+-i/home/ramiro/workspace/markings/2008-05-31-Pizza.jacktool \
+-i/home/ramiro/workspace/markings/2008-08-01-Argentina-album.jacktool \
+-i/home/ramiro/workspace/markings/2008-12-31-Caxambu.jacktool
