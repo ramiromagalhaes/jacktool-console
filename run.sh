28ee1a2c4d61f62ca871fa79d7d26d09e6221aa9
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-10 23:01:23 -0300

    Updated script with a new set of paths.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-./jacktool-console -o/mnt/{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39} \
+./jacktool-console -o/mnt/fundo \
 -i/home/ramiro/workspace/markings/2006-12-17-Zoologico.jacktool \
 -i/home/ramiro/workspace/markings/2007-08-04-IlhaGrande.jacktool \
 -i/home/ramiro/workspace/markings/2009-03-08-Escalada.jacktool \
